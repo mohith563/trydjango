14fd4d646bde1828677b04d5e25c249ed25ac631
mohith563 <ramoh563@gmail.com>
2020-06-21 19:22:08 +0530

    editted print statement in settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
+
 # c:\Users\ST\Dev\trydjango\src
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    #testing commit
     #added products app
 ]
 
